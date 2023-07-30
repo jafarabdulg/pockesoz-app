e224a5c006f6be371eb02c3a07ab061fdeda712a
jafarabdulghoffar <jafarabdulghoffar@gmail.com>
2023-07-30 22:36:50 +0700

    spring boot init

---
+++ src/main/java/com/jafar/pockesoz/PockesozApplication.java
@@ -0,0 +1,13 @@
+package com.jafar.pockesoz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PockesozApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PockesozApplication.class, args);
+    }
+
+}
